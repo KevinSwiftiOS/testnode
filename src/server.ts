c4d1476f70b11844f90e3094326827a072a8df7e
Kevin Swift <17816869731@163.com>
2023-04-06 18:59:49 +0800

    feat: 测试

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,65 @@ import Router from '@koa/router'
 import Redis from 'ioredis';
 import mongoose from 'mongoose';
 import assert from "assert";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import { baseDb, db } from '@open-dy/cloud-database';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import axios from 'axios';
+// import axios from 'axios';
+class Request {
+  // constructor(config: { envID: string; throwOnNotFound: boolean }) {
+  // }
+
+  // eslint-disable-next-line class-methods-use-this
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async send(action: unknown, data: unknown) {
+    const params = Object.assign({}, data, {
+      action,
+    });
+    console.log('请求的参数', params);
+    console.log(
+      process.env.NODE_ENV === 'development'
+        ? 'dycloud-api-boe.byted.org'
+        : 'dycloud.volces.com'
+    );
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'post',
+            url: process.env.NODE_ENV !== 'development'
+                ? 'http://dycloud-api-boe.byted.org/api/cloud_db/exec_cloud_database'
+                : 'http://dycloud.volces.com/api/cloud_db/exec_cloud_database',
+            data: params,
+            headers: {
+                'X-TT-ENV': 'boe_cloud_database_dyc',
+                'content-type': 'application/json',
+            },
+        })
+            .then((res) => {
+                // console.log('res', res);
+            resolve(res);
+        })
+            .catch((error) => {
+            // console.log('error', error);
+            reject(error);
+        });
+    });
+  }
+}
+export class CloudBase {
+  config: any;
+  /* eslint-disable new-cap */
+  constructor(config?: any) {
+    this.config = config;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  database(config: any) {
+    const dbInstance = new db(config);
+    baseDb.reqClass = Request;
+    return dbInstance;
+  }
+}
 
 // 初始化各服务的连接 redis, mongo
 async function initService() {
@@ -40,7 +99,12 @@ initService().then(async ({ redis, mongoose}) => {
     const router = new Router();
     router.get('/', ctx => {
         ctx.body = `Nodejs koa demo project`;
-    }).get('/api/get_data_from_redis', async(ctx) => {
+    }).get('/api/testdb', async(ctx) => {
+       const dbInstance = new CloudBase();
+       const db =  dbInstance.database({ config:true });
+       return db.collection("todos").get();
+    })
+    .get('/api/get_data_from_redis', async(ctx) => {
         const key = ctx.query.key as string;
         assert(key?.trim(), `key is required`);
         const value = await redis.get(key);
