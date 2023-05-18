c6cc43169b837678a743916790b01f9bbb86152a
caokaiqiang <caokaiqiang@bytedance.com>
2023-05-18 15:43:49 +0800

    测试添加
    测试添加

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ initService().then(async () => {
 
    router.post('/api/post',async(ctx) => {
     console.log('1234567',ctx.request.body);
-        const res = await dbInstance.collection("collection2").add({data: {name: ctx.params}});
+        const res = await dbInstance.collection("collection2").add({data: {name: 'caokaiqiang',age: 28, school: 'hangzhou'}});
         ctx.body = res;
         return  ctx.request.body; 
      });
