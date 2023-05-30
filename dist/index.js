bcc673b9a4836a0d8186c3256772a1fc9d5379c1
caokaiqiang <caokaiqiang@bytedance.com>
2023-05-30 10:48:48 +0800

    ceshi

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,19 @@ import Router from '@koa/router'
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 // import axios from 'axios';
-import { dySDK } from './ckq-test-node/dist/commonjs/index.js';
+import { dySDK } from '@open-dy/node-server-sdk';
 async function initService() {
 }
 initService().then(async () => {
     const app = new Koa();
     const router = new Router();
   
-    router.get('/api/test', async(ctx) => {
+    router.get('/api1/test', async(ctx) => {
         console.log("test拿到的结果", 'test');
         ctx.body = 'test';
         return 'test';
      });
-    router.get('/api/get', async (ctx) => {
+    router.get('/api1/get', async (ctx) => {
        console.log("get拿到的结果", ctx.request.body);
        const res = await dySDK.database().collection('todos').get();
        console.log('res', res);
@@ -22,13 +22,13 @@ initService().then(async () => {
        return '11 get';
     });
 
-   router.post('/api/post',async(ctx) => {
+   router.post('/api1/post',async(ctx) => {
     console.log('/api/post',ctx.request.body);
         const res = await dySDK.database().collection("collection2").add({data: {name: 'caokaiqiang',age: 28, school: 'hangzhou'}});
         ctx.body = res;
         return  ctx.request.body; 
      });
-     router.post('/api/add',async(ctx) => {
+     router.post('/api1/add',async(ctx) => {
         console.log('/api/add',ctx.request.body);
             const res = await dySDK.database().collection("collection2").add(ctx.request.body);
             ctx.body = res;
